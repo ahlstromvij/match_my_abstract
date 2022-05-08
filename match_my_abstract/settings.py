9cad6897092406c8c661511023c65334c18fdab1
Kristoffer Ahlstrom-Vij <ahlstromvij@Kristoffers-MacBook-Pro.local>
2022-05-08 22:16:50 +0100

    More Heroku config

--- match_my_abstract/settings.py
+++ match_my_abstract/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,4 +155,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Configure Django App for Heroku.
+import django_on_heroku
 django_on_heroku.settings(locals())
